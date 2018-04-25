cc03a5818132b44043cdb6ecddee8fb80ece3b04
AmirAtPerfecto <amirr@perfectomobile.com>
2018-04-25 19:21:21 +0200

    Example for native app testing on iOS calculator

---
+++ specs/iOSCalculator.spec
@@ -0,0 +1,18 @@
+Getting Started with iOS Native App: Calculator
+===============================
+
+This is an executable specification file. This file follows markdown syntax. Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+To execute this specification, use `mvn test`
+
+* Open the calculator
+
+Get Started
+-----------
+* I press "clear"
+* I press "1"
+* I press "add"
+* I press "2"
+* I press "equals"
+* The result should be "3"
+
+
---
+++ src/test/java/iOSCalculator.java
@@ -0,0 +1,71 @@
+import com.perfecto.reportium.test.TestContext;
+import com.thoughtworks.gauge.Step;
+import driver.DriverFactory;
+import org.openqa.selenium.remote.DriverCommand;
+import org.openqa.selenium.remote.RemoteExecuteMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class iOSCalculator {
+
+    public void iOSCalculator() {
+        DriverFactory.getReportiumClient().testStart("Example Perfecto Gauge iOS Calculator", new TestContext("iOS Calculator", "Gauge"));
+    }
+
+
+    private void setTestResultFailed(Exception e){
+        DriverFactory.setExceptionInformation(e);
+    }
+
+    @Step("Open the calculator")
+    public void launchCalculator() {
+        try {
+            launchApp(DriverFactory.getDriver(), "Calculator");
+        } catch (Exception e) {
+            setTestResultFailed(e);
+            e.printStackTrace();
+        }
+    }
+
+
+    @Step("I press <term>")
+    public void clickElement(String term) {
+        try {
+            switchToContext(DriverFactory.getDriver(), "NATIVE_APP");
+//            driver.findElementByXPath("//*[@label=\"clear\"]").click();
+//            driver.findElementByXPath("//*[@label=\"1\"]").click();
+
+            DriverFactory.getDriver().findElementByXPath("//*[@label=\""+term+"\"]").click();
+        } catch (Exception e) {
+            setTestResultFailed(e);
+            e.printStackTrace();
+        }
+    }
+
+    @Step("The result should be <term>")
+    public void validateResult(String term) {
+
+        try {
+            switchToContext(DriverFactory.getDriver(), "NATIVE_APP");
+            DriverFactory.getDriver().findElementByXPath("//*[@label=\""+term+"\"]");
+        } catch (Exception e) {
+            setTestResultFailed(e);
+            e.printStackTrace();
+        }
+    }
+    //Launches application
+    private static String launchApp(RemoteWebDriver driver, String app) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", app);
+        return (String) driver.executeScript("mobile:application:open", params);
+    }
+    private static void switchToContext(RemoteWebDriver driver, String context) {
+        RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+        Map<String,String> params = new HashMap<String,String>();
+        params.put("name", context);
+        executeMethod.execute(DriverCommand.SWITCH_TO_CONTEXT, params);
+    }
+
+}
